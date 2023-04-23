1a4b9c073b34edec93c0dfd8bff6e8524964807a
Anas8e <bellacheanas@gmail.com>
2023-04-23 10:07:52 -0700

    fix test_1

--- _printf.c
+++ _printf.c
@@ -1,61 +1,68 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * _printf - prints output according to a format.
- * @format: character string.
+ * _printf - Prints formatted output to stdout.
+ * @format: A format string.
  *
- * Return: the number of characters printed.
+ * Return: The number of characters printed (excluding the null byte used to end output to strings).
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, j = 0, count = 0;
-	char *str;
+	int i, count = 0;
+	char *str_arg;
+	char char_arg;
 
 	va_start(args, format);
 
-	while (format && format[i])
+	/* Loop over format string */
+	for (i = 0; format[i] != '\0'; i++)
 	{
+		/* If '%' is encountered, parse the conversion specifier */
 		if (format[i] == '%')
 		{
 			i++;
+
+			/* Handle conversion specifier */
 			switch (format[i])
 			{
-			case 'c':
-				j = va_arg(args, int);
-				write(1, &j, 1);
-				count++;
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (str[j])
-				{
-					write(1, &str[j], 1);
-					j++;
+				case 'c':
+					char_arg = va_arg(args, int);
+					putchar(char_arg);
 					count++;
-				}
-				break;
-			case '%':
-				write(1, &format[i], 1);
-				count++;
-				break;
-			default:
-				write(1, "%", 1);
-				count++;
-				break;
+					break;
+				case 's':
+					str_arg = va_arg(args, char *);
+					while (*str_arg != '\0')
+					{
+						putchar(*str_arg);
+						str_arg++;
+						count++;
+					}
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(format[i]);
+					count += 2;
+					break;
 			}
 		}
 		else
 		{
-			write(1, &format[i], 1);
+			/* Print literal character */
+			putchar(format[i]);
 			count++;
 		}
-		i++;
 	}
+
 	va_end(args);
 
-	return (count);
+	return count;
 }
 
